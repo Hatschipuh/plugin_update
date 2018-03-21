51ecfcbbda449f769f7019b579bcf5813263000b
Hatschipuh <34626930+Hatschipuh@users.noreply.github.com>
2018-03-21 17:39:36 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -8,11 +8,11 @@ Autor:      Hertste, Germany, stefan.programmiert@web.de
 Website:    Http://www.pc.hertste.de
 Copyright:  2018 Stefan H.
 
-Für Bludit Version 2.X
+FÃ¼r Bludit Version 2.X
 
 Installiert automatisch alle aktuellen Plugins und Themes, die auf der Bludit Seite angeboten werden.
 
-Fragen, Wünsche, Anregungen sind erwünscht
+Fragen, WÃ¼nsche, Anregungen sind erwÃ¼nscht
 */
 
 
@@ -38,30 +38,31 @@ class auto_update_for_plugin_and_themes extends Plugin {
 	public function form()
 	{
 		global $Language;
-		//$html = $Language->get('installation').'<br /><br />';  //nicht notwendig?!
-		$html ='<label>Automatically installs all the latest plugins and themes offered on the Bludit site. Warning: This overwrite your plugins/themes when exists.</label><br />';
- 
- 
-        // === Zählener an / aus
-        if ($this->getValue('an_aus') == "1") { //eingeschaltet
-           
-           include("auto_update.php");
-           
-        } 
 
- 
-                   
+		//$html = $Language->get('installation').'<br /><br />';  //nicht notwendig?!
+		$html ='<label>Automatically installs all the latest Bludit Version, plugins and themes offered on the Bludit site. Warning: This overwrite your bludit/plugins/themes when exists.</label><br />';
+
+
+        // === ZÃ¤hlener an / aus
+        if ($this->getValue('an_aus') == "1") { //eingeschaltet
+
+           include("auto_update.php");
+
+        }
+
+
+
         // === Ein- Ausschalten
 		$html .= '<br /><div>';
 		$html .= '<label>Automatically start</label><br>';
-		$html .= '<label>If ON then the plugins and themes are updated automatically when you call this page.</label><br />';
+		$html .= '<label>If ON then the Bludit, plugins and themes are updated automatically when you call this page.</label><br />';
 		$html .= '<select name="an_aus" style="width:100px">';
 		$html .= '<option value="true" '.($this->getValue('an_aus')===true?'selected':'').'>On</option>';
 		$html .= '<option value="false" '.($this->getValue('an_aus')===false?'selected':'').'>Off</option>';
 		$html .= '</select> ';
 		$html .= '</div><br />';
 
-        	
+
 
 
 		return $html;
